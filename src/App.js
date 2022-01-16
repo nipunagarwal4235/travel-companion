dabedc7e0112219aae140ade2852b7b97839b647
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2022-01-16 11:46:25 +0530

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import Header from './Components/Header/Header'
-import List from './Components/List/List'
-import Map from './Components/Map/Map'
+import React from "react";
+
+import { CssBaseline, Grid } from "@material-ui/core";
+
+import Header from "./Components/Header/Header";
+import List from "./Components/List/List";
+import Map from "./Components/Map/Map";
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
-    )
-}
+  return (
+    <>
+      <CssBaseline></CssBaseline>
+        <Header></Header>
+        <Grid container spacing={3}
+    </>
+  );
+};
 
-export default App
+export default App;
